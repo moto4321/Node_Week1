1ca9329ee385af54c8f88caf108e06ce5fbafb04
moto4321 <moto432195@gmail.com>
2022-04-08 17:07:05 +0900

    post CRUD

--- routes/Post.js
+++ routes/Post.js
@@ -7,7 +7,7 @@ const path = require('path')
 
 // 전체 게시물 가져오기
 router.get('/', async (req, res) => {
-  const posts = await Post.find();
+  const posts = await Post.find({ deleted: 0 });
   res.json(posts)
 })
 
@@ -16,8 +16,11 @@ router.get('/:id', async (req, res) => {
   const id = parseInt(req.params.id)
   const post = await Post.findOne({ id: id });
   // res.send(post.html)
-  // res.json(post)
-  res.sendFile(path.join(__dirname, '../client/post.html'), post);
+  res.json({post})
+  // res.sendFile(path.join(__dirname, '../client/post.html'), post);
+  // res.render(path.join(__dirname + '../client/post.html'), {
+  //   post: post
+  // })
 })
 
 // 게시물 하나 올리기
@@ -38,14 +41,33 @@ router.post('/', async (req, res) => {
 })
 
 //게시물 하나 삭제
-router.post('/:id', (req, res) => {
+router.post('/:id', async (req, res) => {
+  const postId = req.params.id
+  await Post.updateOne({ id: postId }, {$set: { deleted: 1 }})
 
+  res.json({ success: true })
 })
 
+// 수정할 게시물 data
+router.get('/update/:id', async (req, res) => {
+  const postId = parseInt(req.params.id)
+
+  const post = await Post.findOne({ id: postId })
+
+  res.json({post})
+})
 
 //게시물 수정
-router.patch('/:id', (req, res) => {
+router.patch('/:id', async (req, res) => {
+  // console.log(req.body)
+  const { title, contents } = req.body
+  const postId = parseInt(req.params.id)
 
+  await Post.updateOne({ id: postId }, {$set: { title: title, contents: contents }})
+  
+  const post = await Post.findOne({ id: postId })
+  // console.log(post)
+  res.json(post)
 })
 
 module.exports = router;
